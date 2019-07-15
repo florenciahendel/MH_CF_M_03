d274a14cb42662bcdddaedbff93f31c36a12a7be
FlorenciaHendel <florenciahendel@gmail.com>
2019-07-15 13:23:20 -0300

    Agrego theme

---
+++ Task_02/VueDarkTheme/src/pages/TestPage.vue
@@ -0,0 +1,39 @@
+<template>
+ <div class="row">
+    <h1>This is a testing page with Vue</h1>
+    <div class="col-md-4">
+      <user-card :user="user"></user-card>
+    </div>
+  </div>
+</template>
+<script>
+ 
+  import UserCard from './Profile/UserCard'
+  export default {
+    components: {
+           UserCard
+    },
+    data() {
+      return {
+        model: {
+          company: 'MindHub',
+          email: 'flor@email.com',
+          username: 'flor1234',
+          firstName: 'Florencia',
+          lastName: 'Hendel',
+          address: 'Dorrego 2124',
+          city: 'Buenos Aires',
+          country: 'Argentina',
+          about: 'Blablabla'
+        },
+        user: {
+          fullName: 'Florencia Hendel',
+          title: 'Trainee',
+          description: `Is that you Mr. Stark?`,
+        }
+      }
+    }
+  }
+</script>
+<style>
+</style>
